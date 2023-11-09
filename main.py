c0516f3f6f8b18e73cf660fdd8cdac2461d699fd
carolus21rex <85852042+carolus21rex@users.noreply.github.com>
2023-11-09 16:28:06 -0500

    Refactoring into camera.py is complete. Build_camera_image needs the camera capture as a parameter to work correctly. For optimization reasons, camera capture is not handled in camera.py. TBD where capture initialization will be handled (in theory it could be a global variable). Making the tests in src/test/test_camera.py next.

--- main.py
+++ main.py
@@ -1,83 +1,19 @@
 import cv2
 import numpy as np
-
-
-def add_rectangles_to_image(image, selected_rectangle=1):
-    # Draw rectangles at the bottom
-    rectangle1_color = (0, 0, 255)  # Red
-    rectangle2_color = (0, 255, 0)  # Green
-    height, width, _ = image.shape
-    rectangle1_bottom = (50, height - 50)
-    rectangle2_bottom = (width - 50, height - 50)
-
-    cv2.rectangle(image, (rectangle1_bottom[0], rectangle1_bottom[1] - 150),
-                  (rectangle1_bottom[0] + 150, rectangle1_bottom[1]), rectangle2_color,
-                  2)  # not selected
-    cv2.rectangle(image, (rectangle2_bottom[0] - 150, rectangle2_bottom[1] - 150),
-                  (rectangle2_bottom[0], rectangle2_bottom[1]), rectangle2_color,
-                  2)  # not selected
-
-    # selected
-    if selected_rectangle == 1:
-        cv2.rectangle(image, (rectangle1_bottom[0], rectangle1_bottom[1] - 150),
-                      (rectangle1_bottom[0] + 150, rectangle1_bottom[1]), rectangle1_color,
-                      -1)
-    elif selected_rectangle == 2:
-
-        cv2.rectangle(image, (rectangle2_bottom[0] - 150, rectangle2_bottom[1] - 150),
-                      (rectangle2_bottom[0], rectangle2_bottom[1]), rectangle1_color,
-                      -1)
-
-    cv2.putText(image, "Button 1", (rectangle1_bottom[0] + 10, rectangle1_bottom[1] - 75),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-    cv2.putText(image, "Button 2", (rectangle2_bottom[0] - 140, rectangle2_bottom[1] - 75),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-    return image
-
-
-def interpret_keypress_as_mode(mode, key):
-    # shift for readability
-    key -= 48
-
-    print(f"check: {key}")
-    # if the user presses a value that we don't care about (aka not a number) do nothing
-    if 0 > key or key >= 10:
-        return mode
-    return int(key)
-
+from src.app import camera
 
 # Create a window and display the image with rectangles
 cv2.namedWindow("Image with Rectangles")
-width, height = 640, 480
-selected_mode = 1
 
-# blank screen for debugging purposes
-blank = np.ones((height, width, 3), dtype=np.uint8) * 255
 
 # Initialize the camera capture
 cap = cv2.VideoCapture(0)
-
+mode = 0
 while True:
 
-    ret, frame = cap.read()
-
-    if not ret:
-        print("Error: Couldn't read a frame.")
+    if camera.build_camera_image(cap) == -1:
         break
 
-    # Call the function to add rectangles to the image with a selected rectangle (1 or 2)
-    image = add_rectangles_to_image(frame, selected_mode)
 
-    # Create a window and display the image with rectangles
-    cv2.imshow("Image with Rectangles", image)
-
-    key = cv2.waitKey(1)
-    if key & 0xFF == ord('q'):
-        break
-
-    if key != -1:
-        selected_mode = interpret_keypress_as_mode(selected_mode, key)
-
-    # Wait for a key press and then close the window
 cv2.waitKey(1)
 cv2.destroyAllWindows()
---
+++ src/app/camera.py
@@ -0,0 +1,73 @@
+import cv2
+
+# Global parameters: List of mode strings, current selected mode
+modes = ["Mode 1", "Mode 2", "Mode 3"]
+mode = -1
+
+
+def add_rectangles_to_image(image):
+    # Define colors
+    unselected_color = (0, 255, 0)  # Green
+    selected_color = (0, 0, 255)  # Red
+
+    # Get the number of modes and calculate button properties
+    num_modes = len(modes)
+    button_width = image.shape[1] // num_modes
+    button_height = 40
+    button_y = image.shape[0] - button_height
+
+    for i, mode_text in enumerate(modes):
+        # Determine button position
+        button_x = i * button_width
+
+        # Draw the button (unselected)
+        cv2.rectangle(image, (button_x, button_y), (button_x + button_width, button_y + button_height),
+                      unselected_color, 2)
+
+        # Draw the button (selected) based on the selected mode
+        if i == mode:
+            cv2.rectangle(image, (button_x, button_y), (button_x + button_width, button_y + button_height),
+                          selected_color, -1)
+
+        # Add mode labels
+        label_x = button_x + 10
+        label_y = button_y + 25
+        cv2.putText(image, mode_text, (label_x, label_y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+
+    return image
+
+
+def interpret_keypress_as_mode(key):
+    global mode
+    # shift for readability
+    key -= 48
+
+    print(f"check: {key} : {mode}")
+    # if the user presses a value that we don't care about (aka not a number) do nothing
+    if 0 > key or key >= 10:
+        return mode
+    return int(key-1)
+
+
+def build_camera_image(capture):
+    global mode
+    ret, frame = capture.read()
+
+    # prevent rubbish input
+    if not ret:
+        print("Error: Couldn't read a frame.")
+        return -1
+
+    # handle camera mode functionality
+    key = cv2.waitKey(1)
+    if key & 0xFF == ord('q'):
+        return -1
+
+    # no key is -1
+    if key != -1:
+        mode = interpret_keypress_as_mode(key)
+
+    image = add_rectangles_to_image(frame)
+
+    # Create a window and display the image with rectangles
+    cv2.imshow("Image with Rectangles", image)
