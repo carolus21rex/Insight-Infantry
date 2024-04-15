52503bddbcd3622eacfd14cbefb13a00b256ab18
carolus21rex <85852042+carolus21rex@users.noreply.github.com>
2024-04-15 17:15:22 -0400

    AI v3 ready to train.

--- src/app/train/YOLO.py
+++ src/app/train/YOLO.py
@@ -7,26 +7,30 @@ import subprocess
 yolov5_loc = os.path.join(os.getcwd(), "yolov5")
 data_loc = os.path.join(os.getcwd(), "data")
 models_loc = os.path.join(os.getcwd(), "models")
+hyp_yaml = os.path.join(os.getcwd(), 'hyp.scratch.yaml')  # Make sure that hyp.scratch.yaml is properly edited
 
-# Make sure 'yolov5s' is downloaded
+# Here we check if CUDA is available and set our device
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+# Loading the model
 model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
 
-# Define your training command
-train_cmd = f"python {os.path.join(yolov5_loc, 'train.py')} --img 640 --batch 16 --epochs 5"
-# Point to your custom data configuration, model configuration, weights, etc.
-train_cmd += f" --data {os.path.join(data_loc, 'data.yaml')} --cfg {os.path.join(yolov5_loc, 'models', 'yolov5s.yaml')} --weights '' --name yolov5s_results"
+# Move the model to the device
+model = model.to(device)
 
-# Run the training command
+# Remove the loop over folds as you're not doing cross validation anymore
+data_yaml = os.path.join(data_loc, 'data.yaml')  # assuming data.yaml is your data file
+train_cmd = (f"python {os.path.join(yolov5_loc, 'train.py')} --img 640 --batch 16 --epochs 5 "
+             f"--data {data_yaml} --cfg {os.path.join(yolov5_loc, 'models', 'yolov5s.yaml')} "
+             f"--weights '' --hyp {hyp_yaml} --lr 0.001 --name yolov5s_results")
 subprocess.run(train_cmd, shell=True)
 
 # Your trained model will be saved under `runs/train/yolov5s_results`
 # Afterwards, you can copy it to the location you want:
-trained_model_src = os.path.join(yolov5_loc, "runs", "train", "yolov5s_results", "weights", "best.pt")
+trained_model_src = os.path.join(yolov5_loc, "runs", "train", "yolov5s_results")
 
 # Be sure models_loc directory exists
 if not os.path.exists(models_loc):
     os.makedirs(models_loc)
-shutil.copy(trained_model_src, models_loc)
 
-# Print the best model name
-print('The best model is:', os.path.join(models_loc, 'best.pt'))
+shutil.move(trained_model_src, models_loc)
